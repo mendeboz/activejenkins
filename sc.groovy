167e6cf8edf95b6ec8e4eff342fdd555fb208a04
mende <mendeboz@hotmail.com>
2023-09-05 11:55:29 +0000

    add javaapp to project

--- sc.groovy
+++ sc.groovy
@@ -1,18 +1,30 @@
-def gitClone = "git clone https://github.com/mendeboz/activejenkins.git /home/vagrant/git/activejenkins"
-println(gitClone)
-gitClone.execute();
-// gitClone.waitForOrKill(100)
-        
-def gitUpdate = "git -C /home/vagrant/git/activejenkins pull".execute();
-// gitUpdate.waitForOrKill(100)
-        
-def gitCheckout = "git -C /home/vagrant/git/activejenkins checkout master".execute(); 
-// gitCheckout.waitForOrKill(100) 
 
-def list = ['/bin/sh', '-c', "sed -n ' /\\[dnsmasq]/ , /\\[/ {/\\[/!p} ' /home/vagrant/git/activejenkins/hosts.ini"]
-list= list.execute().text.split('\n');
+import groovy.yaml.YamlSlurper
+
+def tg = "/home/vagrant/activejenkins/bsmx-ansible-inventory/inventories/qa/group_vars/all/jenkins_jobs_deployments.yml"
+
+def yml = new YamlSlurper().parse( tg as File)
+
+def deployment_group = yml.jenkins_deploy_jobs.find {it.value = 'Portal'}.deployment_group
 
 
 
 
-println (list.flatten())
+
+// example.each { println it }
+
+
+
+//println list
+
+// def JOB_NAME= "portal-service"
+// def list = ['/bin/sh', '-c', "sed -n ' /\\["+JOB_NAME+"]/ , /\\[/ {/\\[/!p} ' /home/vagrant/activejenkins/hosts.ini"]
+// list= list.execute().text.split('\n');
+
+
+
+
+// println (list.flatten())
+
+// def list = ['/bin/sh', '-c', "sed -n ' /\\[portal-service]/ , /\\[/ {/\\[/!p} '  /home/vagrant/activejenkins/hosts.in"]
+// list= list.execute().text.split('\n'); 
